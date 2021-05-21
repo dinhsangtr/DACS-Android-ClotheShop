2543b8732e4db878bffac371cb7449c6bc434ef1
D.S <sangtruong2811@gmail.com>
2021-05-21 16:42:34 +0700

    Add Navigation Bottom Add 4 Fragment for nav_

--- app/src/main/java/com/example/shop/MainActivity.java
+++ app/src/main/java/com/example/shop/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.shop;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActionBar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = getSupportActionBar();
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()){
+                case R.id.nav_home:
+                    return true;
+                case R.id.nav_order:
+                    return true;
+                case R.id.nav_help:
+                    return true;
+                case R.id.nav_user:
+                    return true;
+            }
+            return false;
+        }
+    };
 }
